ef5df27ff94ee23a0d3c70a67e9eb858fa5913e5
ikennaokechukwu71-arch <ikennaokechukwu71@email.com>
2026-05-27 07:41:27 +0100

    fix: simplify image domains, add public RLS policies

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '*.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'msrhzfetnqiqakpvyrik.supabase.co',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
